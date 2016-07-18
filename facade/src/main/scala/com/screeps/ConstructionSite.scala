25bde0ca7eb24cc3c98451c82c3d762830e720ed
John Eckhart <jeckhart@yodle.com>
2016-07-18 15:17:33 -0400

    Add ConstructionSite trait

---
+++ facade/src/main/scala/com/screeps/ConstructionSite.scala
@@ -0,0 +1,28 @@
+package com.screeps
+
+import scala.scalajs.js
+
+@js.native
+trait ConstructionSite extends RoomObject {
+
+  /** A unique object identificator. You can use Game.getObjectById method to retrieve an object instance by its id. */
+  val id: String = js.native
+
+  /** Whether this is your own construction site. */
+  val my: Boolean = js.native
+
+  /** The site's owner */
+  val owner: Owner = js.native
+
+  /** The current construction progress. */
+  val progress: Int = js.native
+
+  /** The total construction progress needed for the structure to be built. */
+  val progressTotal: Int = js.native
+
+  /** One of the STRUCTURE_* constants. */
+  val structureType: StructureType.Value = js.native
+
+  /** Remove the construction site. */
+  def remove(): Errors.Value = js.native
+}
