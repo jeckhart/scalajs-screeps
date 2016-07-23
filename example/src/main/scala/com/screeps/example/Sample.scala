e3166b8ec682d193f14a5e4b504dc063c7b25f05
John Eckhart <jweckhart@gmail.com>
2016-07-23 09:15:29 -0400

    Add tutorial two logic to upgrade controller to the sample

--- example/src/main/scala/com/screeps/example/Sample.scala
+++ example/src/main/scala/com/screeps/example/Sample.scala
@@ -11,17 +11,39 @@ object Sample {
   @JSExport
   def loop() = {
     Game.creeps.values.map( creep => {
-      if (creep.carry("energy") < creep.carryCapacity) {
-        val sources = creep.room.find(FindType.Sources.id).asInstanceOf[js.Array[Source]]
-        if (creep.harvest(sources.head) == Errors.NotInRange.id) {
-          creep.moveTo(sources(0).pos)
-        }
-      } else {
-        if (creep.transfer(Game.spawns("Spawn1"), ResourceType.Energy.toString) == Errors.NotInRange.id) {
-          creep.moveTo(Game.spawns("Spawn1").pos)
-        }
+      creep.memory.role.asInstanceOf[String] match {
+        case "harvester" => harvester(creep)
+        case "upgrader"  => upgrader(creep)
+        case unknown     => g.c.log("unknown role: " + unknown)
       }
     })
   }
+
+  def harvester(creep: Creep) = {
+    if (creep.carry("energy") < creep.carryCapacity) {
+      val sources = creep.room.find(FindType.Sources.id).asInstanceOf[js.Array[Source]]
+      if (creep.harvest(sources.head) == Errors.NotInRange.id) {
+        creep.moveTo(sources(0).pos)
+      }
+    } else {
+      if (creep.transfer(Game.spawns("Spawn1"), ResourceType.Energy.toString) == Errors.NotInRange.id) {
+        creep.moveTo(Game.spawns("Spawn1").pos)
+      }
+    }
+  }
+
+  def upgrader(creep: Creep) = {
+    if (creep.carry("energy") == 0) {
+      val sources = creep.room.find(FindType.Sources.id).asInstanceOf[js.Array[Source]]
+      if (creep.harvest(sources(0)) == Errors.NotInRange.id) {
+        creep.moveTo(sources(0).pos)
+      }
+    } else {
+      if(creep.upgradeController(creep.room.controller) == Errors.NotInRange.id) {
+        creep.moveTo(creep.room.controller.pos)
+      }
+    }
+  }
+
 }
 
